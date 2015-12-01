94dc20726d4e8d30d1a79d6f8d232f40eaafc1c3
mangpo <mangpo@eecs.berkeley.edu>
2015-12-01 15:58:40 -0800

    score: support Categorical & beta distirbution, and comparison node

--- synthesis/scoreTest.py
+++ synthesis/scoreTest.py
@@ -27,6 +27,14 @@ def testMoG2():
     plt.show()
 
 def testMoG3():
+    ast = BinExpNode('*',GaussianDistribNode(mu=20,sig=0.1), \
+                     GaussianDistribNode(mu=30,sig=0.1))
+    estimator = ScoreEstimator(None)
+    mog = estimator.visit(ast)
+    print mog
+
+
+def testMoG4():
     ast = BinExpNode('+',\
                      IfNode(BooleanDistribNode(percentTrue=0.9), \
                             GaussianDistribNode(mu=25,sig=7), \
@@ -37,18 +45,46 @@ def testMoG3():
     estimator = ScoreEstimator(None)
     mog = estimator.visit(ast)
     print mog
-
-def testMoG4():
+    
+def testMoG5():
     ast = GaussianDistribNode(mu=GaussianDistribNode(mu=25,sig=7),sig=GaussianDistribNode(mu=1,sig=1))
     estimator = ScoreEstimator(None)
     mog = estimator.visit(ast)
     print mog
 
-def testMoG5():
+def testMoG6():
     ast = BoolBinExpNode('||', BooleanDistribNode(percentTrue=0.2), \
                          UnaryExpNode('!', BooleanDistribNode(percentTrue=0.7)))
     estimator = ScoreEstimator(None)
     mog = estimator.visit(ast)
     print mog
 
-testMoG5()
+def testMoG7():
+    ast = BinExpNode('+',BetaDistribNode(alpha=2, beta=3),100)
+    estimator = ScoreEstimator(None)
+    mog = estimator.visit(ast)
+    print mog
+
+def testMoG8():
+    x = CategoricalDistribNode(["a","b","c"],{"a":0.2,"b":0.3,"c":0.5})
+    tests = [ComparisonNode(BooleanDistribNode(percentTrue=0.2), "==", BooleanDistribNode(percentTrue=0.2)), \
+             ComparisonNode(BooleanDistribNode(percentTrue=0.2), "==", True), \
+             ComparisonNode(True, "==", True), \
+             ComparisonNode(True, "==", False), \
+             ComparisonNode(GaussianDistribNode(10,0), "==", GaussianDistribNode(10,0)), \
+             ComparisonNode(GaussianDistribNode(10,1), "==", GaussianDistribNode(10,0)), \
+             ComparisonNode(GaussianDistribNode(10,0), "==", GaussianDistribNode(11,0)), \
+             ComparisonNode(x, "==", x), \
+             ComparisonNode(x, "==", "a"), \
+             ComparisonNode("b", "==", "a"), \
+    ]
+
+    for ast in tests:
+        print "e1 =", ast.node
+        print "e2 =", ast.value
+        estimator = ScoreEstimator(None)
+        mog = estimator.visit(ast)
+        print mog
+        print "-------------------------------------------------------"
+
+testMoG8()
