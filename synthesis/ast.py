50912a2ed4bdcbe011c41766aa7bda0a9e3f7e20
schasins <schasins@gmail.com>
2015-12-11 00:41:08 -0800

    Even farther from 0.

--- synthesis/ast.py
+++ synthesis/ast.py
@@ -578,8 +578,8 @@ class GaussianDistribNode(RealDistribNode):
 		else:
 			self.mu = np.mean(matchingRowsValues)
 			self.sig = np.std(matchingRowsValues)
-			if abs(self.sig) < .000001:
-				self.sig = .000001 # shouldn't be using Gaussian to model constants
+			if abs(self.sig) < .0001:
+				self.sig = .0001 # shouldn't be using Gaussian to model constants
 			self.percentMatchingRows = len(matchingRowsValues)/self.program.dataset.numRows
 
 		if debug: print "concrete: gaussian", self.strings()
