1f1b4c88f219d02ff2762013a01be89698e3b577
schasins <schasins@gmail.com>
2016-03-12 20:18:55 -0800

    Splitting dataset into training and test set.

---
+++ benchmarkSuite/realDatasets/splitIntoTrainingAndTest.py
@@ -0,0 +1,35 @@
+import sys
+import random
+
+inputDatasetFilename = sys.argv[1]
+testingCount = int(sys.argv[2])
+
+lines = open(inputDatasetFilename, "r").readlines()
+
+headers = lines[0]
+data = lines[1:]
+
+numDataPoints = len(data)
+indexes = range(numDataPoints)
+testSetIndexes = random.sample(indexes, testingCount)
+
+testSetIndexes = sorted(testSetIndexes, reverse = True)
+
+testingData = []
+
+for index in testSetIndexes:
+    testingData.append(data[index])
+    del data[index]
+
+def writeSetToFile(filename, headers, data):
+    fileO = open(filename, "w")
+    fileO.write(headers)
+    for line in data:
+        fileO.write(line)
+
+writeSetToFile("trainingData.csv", headers, data)
+print len(data)
+writeSetToFile("testingData.csv", headers, testingData)
+print len(testingData)
+
+print numDataPoints
